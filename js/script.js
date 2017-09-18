b6ca46658de53ac2341115b27a7c7172d2a389e4
Michael Chiang <michael.chiang25@gmail.com>
2017-09-18 17:46:04 -0400

    Fixed javascript

--- js/script.js
+++ js/script.js
@@ -1,30 +1,4 @@
 $(document).ready(function() {
-
-var provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope('profile');
-provider.addScope('email');
-
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-  authorizedRun();
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-
-function authorizedRun(){
-	// Initialize Firebase
     var config = {
         apiKey: "AIzaSyBhPt4WJ0qd2HIS8pQGHhPAbIRyZbwS2u8",
         authDomain: "trainschedulerz.firebaseapp.com",
@@ -34,84 +8,110 @@ function authorizedRun(){
         messagingSenderId: "975014199256"
     };
     firebase.initializeApp(config);
-
-    var database = firebase.database();
-
-    var trainName = "";
-    var destination = "";
-    var freq = "";
-    var firstArrival = "";
-    var nextArrival = "";
-    var minAway = "";
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
 
 
-    $('#submit').on('click', function(e) {
-        e.preventDefault();
-
-        trainName = $('#train_name').val().trim();
-        destination = $('#destination').val().trim();
-        freq = $('#frequency').val().trim();
-        firstArrival = $('#first_train').val().trim();
-        if (trainName != "" && destination != "" && freq != "" && firstArrival != "") {
-            database.ref().push({
-                name: trainName,
-                destination: destination,
-                frequency: freq,
-                firstArrival: firstArrival,
-                nextArrival: "",
-                minutesAway: 0,
-                dateAdded: firebase.database.ServerValue.TIMESTAMP
-            });
-        } else {
-            console.log("Please Fill Required Boxes");
-        }
-
-
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        authorizedRun();
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
     });
 
-    database.ref().orderByChild("name").on("child_added", function(childSnapshot) {
-
-        var tN = childSnapshot.val().name;
-        var tD = childSnapshot.val().destination;
-        var tF = childSnapshot.val().frequency;
-        var tFA = childSnapshot.val().firstArrival;
-        var tDA = childSnapshot.val().dateAdded;
-
-        var tMA = 0;
-        var tNA = "";
+    function authorizedRun() {
+        // Initialize Firebase
 
 
-        tFA = moment(tFA, "hh:mm");
-        var currentTime = moment().format();
-        var diffTime = moment().diff(moment(tFA), "minutes");
-        var remainder = diffTime % tF;
-        tMA = tF - remainder;
-        tNA = moment().add(tMA, "minutes");
-        tNA = moment(tNA).format("hh:mm");
+        var database = firebase.database();
+
+        var trainName = "";
+        var destination = "";
+        var freq = "";
+        var firstArrival = "";
+        var nextArrival = "";
+        var minAway = "";
+
+
+        $('#submit').on('click', function(e) {
+            e.preventDefault();
+
+            trainName = $('#train_name').val().trim();
+            destination = $('#destination').val().trim();
+            freq = $('#frequency').val().trim();
+            firstArrival = $('#first_train').val().trim();
+            if (trainName != "" && destination != "" && freq != "" && firstArrival != "") {
+                database.ref().push({
+                    name: trainName,
+                    destination: destination,
+                    frequency: freq,
+                    firstArrival: firstArrival,
+                    nextArrival: "",
+                    minutesAway: 0,
+                    dateAdded: firebase.database.ServerValue.TIMESTAMP
+                });
+            } else {
+                console.log("Please Fill Required Boxes");
+            }
+
+
+        });
+
+        database.ref().orderByChild("name").on("child_added", function(childSnapshot) {
+
+            var tN = childSnapshot.val().name;
+            var tD = childSnapshot.val().destination;
+            var tF = childSnapshot.val().frequency;
+            var tFA = childSnapshot.val().firstArrival;
+            var tDA = childSnapshot.val().dateAdded;
+
+            var tMA = 0;
+            var tNA = "";
+
+
+            tFA = moment(tFA, "hh:mm");
+            var currentTime = moment().format();
+            var diffTime = moment().diff(moment(tFA), "minutes");
+            var remainder = diffTime % tF;
+            tMA = tF - remainder;
+            tNA = moment().add(tMA, "minutes");
+            tNA = moment(tNA).format("hh:mm");
 
 
 
-        $('#table-body').append("<tr>" + "<td class='t-name'>" + tN + "</td>" +
-            "<td class='destination'>" + tD + "</td>" + "<td class='frequency'>" +
-            tF + "</td>" + "<td class='next-arrival'>" + tNA + "</td>" + "<td class='min-away'>" +
-            tMA + "</td>" + "</tr>");
-    }, function(err) {
-        console.log("Error: " + err);
-    });
+            $('#table-body').append("<tr>" + "<td class='t-name'>" + tN + "</td>" +
+                "<td class='destination'>" + tD + "</td>" + "<td class='frequency'>" +
+                tF + "</td>" + "<td class='next-arrival'>" + tNA + "</td>" + "<td class='min-away'>" +
+                tMA + "</td>" + "</tr>");
+        }, function(err) {
+            console.log("Error: " + err);
+        });
 
-    // setInterval(timeChange, 60000);
+        // setInterval(timeChange, 60000);
 
-    // function timeChange() {
-    //     database.ref().once("child_added", function(snapshot) {
-    //         for (key in snapshot.val()) {
-               
-    //         }
-    //     });
+        // function timeChange() {
+        //     database.ref().once("child_added", function(snapshot) {
+        //         for (key in snapshot.val()) {
 
-    // }
-    // timeChange();
-}
-    
+        //         }
+        //     });
+
+        // }
+        // timeChange();
+    }
+
 
 
 
