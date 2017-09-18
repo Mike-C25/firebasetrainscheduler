06a4281ba9b6db218e8a69a22631e0159fc6011a
Michael Chiang <michael.chiang25@gmail.com>
2017-09-18 17:51:23 -0400

    Fixed auth

--- js/script.js
+++ js/script.js
@@ -13,12 +13,14 @@ $(document).ready(function() {
     provider.addScope('email');
 
 
+    $('#main-body').hide();
     firebase.auth().signInWithPopup(provider).then(function(result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
         // ...
+        $('#main-body').fadeIn();
         authorizedRun();
     }).catch(function(error) {
         // Handle Errors here.
